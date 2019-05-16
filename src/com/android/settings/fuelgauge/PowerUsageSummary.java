960887b543189cc313e331770459243caacd028c
Anushek Prasal <anushekprasal@gmail.com>
2019-05-17 03:35:53 +0530

    Settings: Restart SystemUI when toggling battery estimates

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -45,6 +45,7 @@ import com.android.settings.Utils;
 import com.android.settings.applications.LayoutPreference;
 import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.fuelgauge.BatteryEstimatesPreferenceController;
 import com.android.settings.display.BatteryPercentagePreferenceController;
 import com.android.settings.fuelgauge.anomaly.Anomaly;
 import com.android.settings.fuelgauge.anomaly.AnomalyDetectionPolicy;
@@ -266,6 +267,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 KEY_BATTERY_TIP, (SettingsActivity) getActivity(), this /* fragment */, this /*
                 BatteryTipListener */);
         controllers.add(mBatteryTipPreferenceController);
+        controllers.add(new BatteryEstimatesPreferenceController(context));
         controllers.add(new BatteryPercentagePreferenceController(context));
         return controllers;
     }
