AquariOS/packages_apps_Settings

Long press custom header to open custom header settings [2/2]
Change-Id: I5ce74197ef3a980a937811f8c0fe2d8d2d5020c8

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -142,6 +142,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.aquarios.coralreef.CoralReef;
+import com.aquarios.coralreef.fragments.CustomHeaders;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 public class SettingsGateway {
@@ -274,7 +275,8 @@ public class SettingsGateway {
             SmartbarSettings.class.getName(),
             FlingSettings.class.getName(),
             PulseSettings.class.getName(),
-            SwipeUpGestureSettings.class.getName()
+            SwipeUpGestureSettings.class.getName(),
+            CustomHeaders.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -318,6 +320,7 @@ public class SettingsGateway {
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(), 
             Settings.PulseSettingsActivity.class.getName(),
-            Settings.StockNavigationActivity.class.getName()
+            Settings.StockNavigationActivity.class.getName(),
+            Settings.CustomHeadersActivity.class.getName()
     };
 }

GrazieMaldini/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -140,6 +140,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.dirtyunicorns.tweaks.fragments.CustomHeaders;
 import com.dirtyunicorns.tweaks.fragments.GamingMode;
 import com.dirtyunicorns.tweaks.DirtyTweaks;
 
@@ -270,7 +271,9 @@ public class SettingsGateway {
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             DirtyTweaks.class.getName(),
             RunningServices.class.getName(),
-            GamingMode.class.getName()
+            GamingMode.class.getName(),
+            CustomHeaders.class.getName()
+
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -311,6 +314,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.DirtyTweaksActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
-            Settings.GamingModeActivity.class.getName()
+            Settings.GamingModeActivity.class.getName(),
+            Settings.CustomHeadersActivity.class.getName()
     };
 }